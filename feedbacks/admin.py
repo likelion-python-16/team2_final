de37a5b01cbe2783a41909ef462a9f6fa8526a42
jonghyun <rubms01@gmail.com>
2025-09-18 13:43:10 +0900

    Initial commit

--- feedbacks/admin.py
+++ feedbacks/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from.models import Feedback
+from.models import Feedback,DailyReport,Achievement
 
 # Register your models here.
 admin.site.register(Feedback)
+admin.site.register(DailyReport)
+admin.site.register(Achievement)
